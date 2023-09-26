dc62bcababf20a0463ad61099548c8148ecf3b69
nikita <nikitajbhavsar@gmail.com>
2023-09-26 16:24:31 +0530

    mera first commit

---
+++ StringDemo/src/com/example/StringCompare.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class StringCompare {
+
+	public static void main(String[] args) {
+
+String str1="code";
+
+String str2=new String("code");
+
+if(str1.equals(str2))
+{
+	
+	System.out.println("both strings are equal");
+	
+}else
+{
+	System.out.println("Strings are not equal");
+	
+}
+	}
+
+}
