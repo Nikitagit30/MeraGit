ed42964de851df5d3f2c25ceabbc38b163d8a273
nikita <nikitajbhavsar@gmail.com>
2023-10-06 16:04:41 +0530

    added two new files

---
+++ StringDemo/src/com/example/StringAppend.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class StringAppend {
+
+	public static void main(String[] args) {
+
+String str1="Nik";
+String str2="Jay";
+
+System.out.println("Appending of two string is:" +str1.toUpperCase());
+
+
+System.out.println("Appending of two string is:" +str2.toUpperCase());
+
+	}
+
+}
---
+++ StringDemo/src/com/example/Stringlength.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Stringlength {
+
+	public static void main(String[] args) {
+
+String str1="Nikita";
+
+System.out.println("lenth of the string is:" +str1.length());
+
+	}
+
+}
